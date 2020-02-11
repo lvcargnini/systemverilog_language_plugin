ab59a94df8458ac9f2e5fa2130b0be1a85c2c23b
Luis Vitorio Cargnini <lvcargnini@gmail.com>
2020-02-10 17:35:06 -0800

    adding missing source

--- src/main/java/org/lvcargnini/systemverilog_language_plugin/psi/impl/SystemVerilogNamedElementImpl.java
+++ src/main/java/org/lvcargnini/systemverilog_language_plugin/psi/impl/SystemVerilogNamedElementImpl.java
@@ -1,4 +1,13 @@
 package org.lvcargnini.systemverilog_language_plugin.psi.impl;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.lang.ASTNode;
+import org.lvcargnini.systemverilog_language_plugin.psi.SystemVerilogNamedElement;
+import org.jetbrains.annotations.NotNull;
 
-public class SystemVerilogNamedElementImpl {
+
+public abstract class SystemVerilogNamedElementImpl  extends ASTWrapperPsiElement implements SystemVerilogNamedElement {
+    public SystemVerilogNamedElementImpl(@NotNull ASTNode node) {
+        super(node);
+    }
+
 }
--- src/main/java/org/lvcargnini/systemverilog_language_plugin/psi/impl/SystemVerilogPsiImplUtil.java
+++ src/main/java/org/lvcargnini/systemverilog_language_plugin/psi/impl/SystemVerilogPsiImplUtil.java
@@ -1,4 +1,59 @@
 package org.lvcargnini.systemverilog_language_plugin.psi.impl;
 
+import com.intellij.lang.ASTNode;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.psi.*;
+import org.lvcargnini.systemverilog_language_plugin.SystemVerilogIcons;
+import org.lvcargnini.systemverilog_language_plugin.psi.*;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
 public class SystemVerilogPsiImplUtil {
+
+    public static String getKey(SystemVerilogProperty element) {
+        ASTNode keyNode = element.getNode().findChildByType(SystemVerilogTypes.KEY);
+        if (keyNode != null) {
+            // IMPORTANT: Convert embedded escaped spaces to simple spaces
+            return keyNode.getText().replaceAll("\\\\ ", " ");
+        } else {
+            return null;
+        }
+    }
+
+    public static String getValue(SystemVerilogProperty element) {
+        ASTNode valueNode = element.getNode().findChildByType(SystemVerilogTypes.VALUE);
+        if (valueNode != null) {
+            return valueNode.getText();
+        } else {
+            return null;
+        }
+    }
+    // ...
+
+    public static String getName(SystemVerilogProperty element) {
+        return getKey(element);
+    }
+
+    public static PsiElement setName(SystemVerilogProperty element, String newName) {
+        ASTNode keyNode = element.getNode().findChildByType(SystemVerilogTypes.KEY);
+        if (keyNode != null) {
+
+            SystemVerilogProperty property = SystemVerilogElementFactory.createProperty(element.getProject(), newName);
+            ASTNode newKeyNode = property.getFirstChild().getNode();
+            element.getNode().replaceChild(keyNode, newKeyNode);
+        }
+        return element;
+    }
+
+    public static PsiElement getNameIdentifier(SystemVerilogProperty element) {
+        ASTNode keyNode = element.getNode().findChildByType(SystemVerilogTypes.KEY);
+        if (keyNode != null) {
+            return keyNode.getPsi();
+        } else {
+            return null;
+        }
+    }
+
+    // ...
 }
