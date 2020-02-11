94c379f9bef406e8cfcf27b8df7f10745d317db4
Luis Vitorio Cargnini <lvcargnini@gmail.com>
2020-02-11 10:50:27 -0800

    fixing icon path

--- src/main/java/org/lvcargnini/systemverilog_language_plugin/SystemVerilogIcons.java
+++ src/main/java/org/lvcargnini/systemverilog_language_plugin/SystemVerilogIcons.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class SystemVerilogIcons {
-    public static final Icon FILE = IconLoader.getIcon("/com/velodyne/systemverilogplugin/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/org/lvcargnini/system_verilog_plugin/icons/jar-gray.png");
 
 }
 
